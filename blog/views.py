42e77a7f2eb69947f59b529af7607da8012ef73a
Kevin2Holt <kevin2holt@gmail.com>
2022-12-09 11:41:22 -0500

    Finished main Django tutorial

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 
 def post_list(req):
 	Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
 	return render(req, 'blog/post_list.html', {"posts": posts})
+
+def post_detail(req, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(req, 'blog/post_detail.html', {'post': post})
+
+def post_new(req):
+	if(req.method == "POST"):
+		form = PostForm(req.POST)
+		if(form.is_valid()):
+			post = form.save(commit=False)
+			post.author = req.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+	return render(req, 'blog/post_edit.html', {'form': form})
+
+def post_edit(req, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if(req.method == "POST"):
+		form = PostForm(req.POST, instance=post)
+		if(form.is_valid()):
+			post = form.save(commit=False)
+			post.author = req.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(req, 'blog/post_edit.html', {'form': form})
